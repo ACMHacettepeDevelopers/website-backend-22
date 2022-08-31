037433c922d6dcb0aecdd0bafd75969e6a799075
furkansimsekli <simseklifurkan0@gmail.com>
2022-08-31 22:44:39 +0300

    removed: test lines for seeding

--- models/users.js
+++ models/users.js
@@ -69,80 +69,64 @@ const memberSchema = new mongoose.Schema({
         required: true,
     },
 
+    last_name: {
+        type: String,
+        required: true,
+    },
+
+    student_id: {
+        type: String,
+        required: true,
+    },
+
+    department: {
+        type: String,
+        required: true,
+    },
+
+    year_of_education: {
+        type: String,
+        required: true,
+    },
+
+    email: {
+        type: String,
+        required: true
+    },
+
+    first_choice_coord: {
+        type: String,
+        required: true
+    },
+
+    second_choice_coord: {
+        type: String,
+        required: true
+    },
+
     unit: {
         type: String,
         required: true
+    },
+
+    phone_number: {
+        type: String,
+        required: true
+    },
+
+    date_of_birth: {
+        type: Date
+    },
+
+    password: {
+        type: String,
+        required: true,
     },
 
     is_active_member: {
         type: Boolean
     }
 });
-
-// const memberSchema = new mongoose.Schema({
-//     first_name: {
-//         type: String,
-//         required: true,
-//     },
-
-//     last_name: {
-//         type: String,
-//         required: true,
-//     },
-
-//     student_id: {
-//         type: String,
-//         required: true,
-//     },
-
-//     department: {
-//         type: String,
-//         required: true,
-//     },
-
-//     year_of_education: {
-//         type: String,
-//         required: true,
-//     },
-
-//     email: {
-//         type: String,
-//         required: true
-//     },
-
-//     first_choice_coord: {
-//         type: String,
-//         required: true
-//     },
-
-//     second_choice_coord: {
-//         type: String,
-//         required: true
-//     },
-
-//     unit: {
-//         type: String,
-//         required: true
-//     },
-
-//     phone_number: {
-//         type: String,
-//         required: true
-//     },
-
-//     date_of_birth: {
-//         type: Date
-//     },
-
-//     password: {
-//         type: String,
-//         required: true,
-//     },
-
-//     is_active_member: {
-//         type: Boolean
-//     }
-// });
 
 const Admin = mongoose.model("Admin", adminSchema);
 const Member = mongoose.model("Member", memberSchema);
